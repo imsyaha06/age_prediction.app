8ade15aad1b983aa103de3ef71d8e1d3d59a799a
Sumbal Haroon <tsanamina8@gmail.com>
2024-12-13 01:13:14 -0800

    Update person_age.py

--- person_age.py
+++ person_age.py
@@ -24,8 +24,8 @@ uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png
 if uploaded_file is not None:
     # Display uploaded image
     image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image", use_column_width=True)
-
+    st.image(image, caption="Uploaded Image", use_container_width=True)
+    
     # Preprocess the image for prediction
     preprocessed_image = preprocess_image(image)
 
